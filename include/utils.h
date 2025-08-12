394421adc31d53854792334ef03dc5bf7be9fe46
FerSoriano <fer8f10@hotmail.com>
2025-08-11 21:40:44 -0600

    Refactor utils module: enhance stopwatch functionality and improve input handling

--- include/utils.h
+++ include/utils.h
@@ -1,7 +1,13 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <stack>
+
 void valid_input();
 void clear_console();
+void show_elapsed_time(long total_time, long lap_time);
+void start_stopwatch();
+void clear_stacks();
+void show_stopwatch(const long& total_time, std::stack<int> lap, std::stack<long>split, std::stack<long>total);
 
 #endif
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,13 +1,22 @@
 #include "../include/utils.h"
+#include "../include/stopwatch.h"
+#include "../include/menu.h"
+
 #include <iostream>
 #include <limits>
+#include <utility> // swap
+#include <iomanip>
+#include <stack>
 
-using namespace std;
+int laps = 0;
+std::stack<int> lap_stack;
+std::stack<long> split_stack;
+std::stack<long> total_stack;
 
 void valid_input(){
-    if (!cin.good()){
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    if (!std::cin.good()){
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
 }
 
@@ -18,3 +27,94 @@ void clear_console(){
         system("clear"); // macOS/Linux
     #endif
 };
+
+// TODO: separate into a new module?
+void start_stopwatch(){
+    int _start_menu, _stop_menu;
+    bool running = true;
+    Stopwatch main_sw;
+    Stopwatch lap_sw;
+    while(running){
+        main_sw.start(); // start
+        lap_sw.start();
+        
+        clear_console();
+        std::cout<<"Running..."<<std::endl;
+        show_stopwatch(main_sw.get_elapsed_time(), lap_stack, split_stack, total_stack);
+        _start_menu = start_menu();
+
+        if(_start_menu < 1 || _start_menu > 3){
+            continue;
+        }
+
+        if(_start_menu == 1){ // stop
+            main_sw.stop();
+            lap_sw.stop();
+            while(true){
+                clear_console();
+                std::cout<<"On hold..."<<std::endl;
+                show_stopwatch(main_sw.get_elapsed_time(), lap_stack, split_stack, total_stack);
+                _stop_menu = stop_menu();
+                if(_stop_menu < 1 || _stop_menu > 3){
+                    continue;
+                }
+                
+                if(_stop_menu == 1){ // start
+                    main_sw.start();
+                    lap_sw.start();
+                    break;
+                }
+                
+                if(_stop_menu ==  2){ // reset
+                    main_sw.reset();
+                    lap_sw.reset();
+                    clear_stacks();
+                    running = false;
+                    laps = 0;
+                    break;
+                }
+            }
+        }
+
+        if(_start_menu == 2){ // lap
+            lap_sw.stop();
+            laps++;
+            lap_stack.push(laps);
+            split_stack.push(lap_sw.get_elapsed_time());
+            total_stack.push(main_sw.get_elapsed_time());
+            lap_sw.reset();
+        }
+    }
+    clear_console();
+    std::cout<<"Reset..."<<std::endl;
+    show_stopwatch(main_sw.get_elapsed_time(), lap_stack, split_stack, total_stack);
+
+}
+
+void show_elapsed_time(long total_time, long lap_time){
+    std::cout << "Total elapsed time: " << total_time << " ms\n";
+    std::cout << "Lap elapsed time: " << lap_time << " ms\n";
+}
+
+void clear_stacks(){
+    std::stack<int> empty_lap_stack;
+    std::stack<long> empty_split_stack;
+    std::stack<long> empty_total_stack;
+    std::swap(lap_stack, empty_lap_stack); 
+    std::swap(split_stack, empty_split_stack);
+}
+
+void show_stopwatch(const long& total_time, std::stack<int> lap, std::stack<long>split, std::stack<long>total){
+    std::string lines = "-------------------------------------------------------";
+    std::cout << "\n\n";
+    std::cout << lines << std::endl;
+    std::cout << "\t\tCurrent time: " << total_time << " s." << std::endl;
+    std::cout << lines << std::endl;
+    std::cout << std::left << std::setw(23) << "Lap No." << std::setw(23) << "Split" << std::setw(23) << "Total" << std::endl;
+    while (!lap.empty()) {
+        std::cout << std::left << std::setw(23) << lap.top() << std::setw(23) << split.top() << std::setw(23) << total.top() << std::endl;
+        lap.pop();
+        split.pop();
+        total.pop();
+    }
+}
