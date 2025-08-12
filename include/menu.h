cce4fcc45d24577f337ac0e648b9bfdf338064a2
FerSoriano <fer8f10@hotmail.com>
2025-08-11 21:40:30 -0600

    Implement menu functionality with menu, start_menu, and stop_menu in C++

---
+++ include/menu.h
@@ -0,0 +1,8 @@
+#ifndef MENU_H
+#define MENU_H
+
+int menu();
+int start_menu();
+int stop_menu();
+
+#endif
---
+++ src/menu.cpp
@@ -0,0 +1,39 @@
+#include "../include/menu.h"
+#include "../include/utils.h"
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+
+std::string lines = "-------------------------------------------------------";
+
+int menu(){
+    int option;
+    std::cout << "\n\n";
+    std::cout << lines << std::endl;
+    std::cout << std::left << std::setw(23) << "1) Start" << std::setw(23) << "2) Exit" << std::setw(23) << "Option: " << std::endl;
+    std::cout << lines << std::endl;
+    std::cin >> option; valid_input();
+    return option;
+}
+
+int start_menu(){
+    int option;
+    std::cout << "\n\n";
+    std::cout << lines << std::endl;
+    std::cout << std::setw(15) << "1) Stop" << std::setw(15) << "2) Lap" << std::setw(15) << "3) Refresh" << std::setw(15) << "Option: " << std::endl;
+    std::cout << lines << std::endl;
+    std::cin >> option; valid_input();
+    return option;
+}
+
+int stop_menu(){
+    int option;
+    std::cout << "\n\n";
+    std::cout << lines << std::endl;
+    std::cout << std::setw(15) << "1) Start" << std::setw(15) << "2) Reset" << std::setw(15) << "3) Refresh" << std::setw(15) << "Option: " << std::endl;
+    std::cout << lines << std::endl;
+    std::cin >> option; valid_input();
+    return option;
+}
